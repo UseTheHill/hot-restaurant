1ea5a38617b3a485508242c2e753c234dccf7772
Mike Bussert <mbussert@gmail.com>
2021-04-03 12:46:09 -0400

    Fixed links in all 3 HTML docs, updated server to load home.html

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 // Routes
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "home.html")));
 
 app.get("/tables", (req, res) =>
   res.sendFile(path.join(__dirname, "tables.html"))
